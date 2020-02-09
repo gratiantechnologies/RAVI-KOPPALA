8fffc85bd5a2bb840ab493d6478d6df2e3df2f37
ravikoppalajanaki <ravikoppala@gratiantechnologies.com>
2020-02-09 11:49:23 +0530

    python program for tuples

---
+++ tuples.py
@@ -0,0 +1,18 @@
+tuple1=(12,15,11.11,"ravi","vizag",99.9)
+print(tuple1)
+print(tuple1[2:5])
+print(tuple1[3:])
+tuple2=(99,51,79,69,81,"phone",1.9 )
+print(tuple2)
+print(tuple1 + tuple2)
+print(tuple1*3)
+print(len(tuple1))
+
+tuple3=(22,34,56,78,11,19)
+print(tuple3)
+print(min(tuple3))
+print(max(tuple3))
+
+list1=[12,13,45,67]
+print(list1)
+print(tuple(list1))
