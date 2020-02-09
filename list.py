b66b1a3a136ef6e8a00323f98f9f158afc905d6a
ravikoppalajanaki <ravikoppala@gratiantechnologies.com>
2020-02-09 11:00:57 +0530

    python program for list

---
+++ list.py
@@ -0,0 +1,35 @@
+#mylist=['ravi',11,17,1009,10.2]
+#print(mylist)
+#print(mylist[2])
+
+#mylist[1]=500
+#print(mylist)
+
+#for i in mylist:
+ #   print(i)
+
+#print(len(mylist))
+
+#mylist.append("python")
+#print(mylist)
+
+#del mylist[2]
+#print(mylist)
+
+#mylist.pop()
+#print(mylist)
+
+#mylist.remove(1009)
+#print(mylist)
+
+#mylist.clear()
+#print(mylist)
+
+#del mylist
+#print(mylist)
+
+mylist=list(("white","black","red","blue"))
+print(mylist)
+
+mylist.reverse()
+print(mylist)
