2d4114b7050bd241e33e4219af3503983ea8dd18
ravikoppalajanaki <ravikoppala@gratiantechnologies.com>
2020-02-13 10:31:51 +0530

    python program for functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def sum(x,y):
+    s=x+y
+    print("sum is:",s)
+sum(10,20)
+
+def myfuc():
+    print("hello world")
+myfuc()
+
+list1=['name','roll no','class']
+list2=['ravi',11,'10th']
+list3=[1,2,3]
+print(dict(zip(list1,list2)))
+print(list(zip(list1,list2,list3)))
