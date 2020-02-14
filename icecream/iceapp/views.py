6dc4c9b1a23d8a79b35846fc945d5ac5597813cc
ravikoppalajanaki <ravikoppala@gratiantechnologies.com>
2020-02-14 12:21:58 +0530

    django application

---
+++ icecream/iceapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def emp(request):
+    return render(request, 'HTML 5.html')
+# Create your views here.
